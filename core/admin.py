a6eefab7521234999607d6839b4f734721747430
Leonardo <leonardo.felipe@outlook.com>
2022-03-27 19:58:04 -0300

    Update

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Item
+
+class Items(admin.ModelAdmin):
+    list_display = ('id', 'item_name', 'item_quantity')
+    search_fields = ('item_name',)
+    
+admin.site.register(Item, Items)
+
