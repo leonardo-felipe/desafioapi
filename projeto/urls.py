cd76de62400eb9eedb38061c54132edee6b065bf
Leonardo <leonardo.felipe@outlook.com>
2022-03-27 20:58:31 -0300

    Update

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from core.views import ItemsViewsSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('items', ItemsViewsSet, basename='Items')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include ('core.urls')),
+    path('', include(router.urls)),
 ]
