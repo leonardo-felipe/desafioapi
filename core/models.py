b2db62a638293e9309b3119aa5a537f405e3ae8b
Leonardo <leonardo.felipe@outlook.com>
2022-03-27 16:19:30 -0300

    Initial commit

---
+++ core/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=100)
+    item_quantity = models.IntegerField('Quantity')
+
+def __str__(self):
+    return self.item_name
