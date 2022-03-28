f9b2605f82015def5e7c1649fbc6eb77dd407868
Leonardo <leonardo.felipe@outlook.com>
2022-03-28 10:32:40 -0300

    Update

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('register/', RegisterAPI.as_view(), name='register'),
     path('login/', LoginAPI.as_view(), name='login'),
     path('logout/', knox_views.LogoutView.as_view(), name='logout'),
-    path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
     path('items/', ItemAPIView.as_view(), name='items'),
 ]
