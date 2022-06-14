011823831c7cdc1c826d61261cf2ced63f776292
Leonardo Oliveira <60754433+leonardo-felipe@users.noreply.github.com>
2022-06-13 22:01:20 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView 
 from rest_framework.response import Response
 from rest_framework import generics, permissions
-from rest_framework.response import Response
 from knox.models import AuthToken
 from .serializers import UserSerializer, RegisterSerializer
 from rest_framework import viewsets
